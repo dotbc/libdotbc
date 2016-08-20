75be859f0312fccb7101fcf382e9960590f0bd96
Carl Lerche <me@carllerche.com>
2016-08-19 23:18:02 -0700

    Tweak args

--- src/rs/main.rs
+++ src/rs/main.rs
@@ -4,34 +4,52 @@ extern crate docopt;
 use docopt::Docopt;
 use std::process;
 
-const TOP: &'static str = "
-Usage:
-    dotbc --help
-    dotbc <cmd> [<args>...]
+const USAGE: &'static str = "
+dotbc
 
-";
-/*
+Usage:
+    dotbc <cmd> [<args>...]
+    dotbc (-h | --help)
+
 Options:
     -h, --help      Display this message
+
 The most commonly used commands are:
-    read        Read a file contained by a .bc archive
-    write       Write a file to a .bc archive
+    read-file        Read a file contained by a .bc archive
+    write-file       Write a file to a .bc archive
 ";
-*/
+
+const READ_FILE: &'static str = "
+dotbc read-file
+
+Usage:
+    dotbc read-file <path> (<bc-file> | -)
+";
 
 pub fn main() {
-    let res = Docopt::new(TOP)
+    let res = Docopt::new(USAGE)
         .and_then(|d| d.parse())
-        .unwrap_or_else(|_| {
-            println!("Invalid arguments\n");
-            println!("{}", TOP);
-            process::exit(1);
-        });
+        .unwrap_or_else(|e| e.exit());
 
     if res.get_bool("-h") {
-        println!("{}", TOP);
+        println!("{}", USAGE);
         process::exit(0);
     }
 
-    println!("GOT: {:?}", res);
+    println!("{:?}", res);
+    println!("CMD: {:?}", res.get_str("<cmd>"));
+
+    if res.get_str("<cmd>") == "read-file" {
+        let mut args: Vec<String> = vec!["dotbc".into(), "read-file".into()];
+        args.append(&mut res.get_vec("<args>").iter().map(|&s| s.into()).collect());
+        println!("ARGS: {:?}", args);
+
+        let res = Docopt::new(READ_FILE)
+            .and_then(|d| d.argv(args).parse())
+            ;
+
+        println!("ZOMG: {:?}", res);
+    } else {
+        println!("NOPE");
+    }
 }
